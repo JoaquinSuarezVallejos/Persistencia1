b22219950fc2c8e20279b695be35ba9b7cf2bcd6
PAX-12 <joaquin.suarezvallejos@gmail.com>
2022-11-01 11:39:08 -0300

    Initial commit

---
+++ Assets/Scripts/EnemigoV2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoV2 : MonoBehaviour
+{
+    public EnemigoData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!data.isAlive)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        data.isAlive = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+    }
+}
